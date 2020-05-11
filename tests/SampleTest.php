6c3fdeff6ef21dd034c8816d60274904acfa7044
mwhorse46 <needmefordev@gmail.com>
2020-05-12 01:18:20 +0200

    Add PHPUnit and a sample test

---
+++ tests/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SkeletonTest;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testSample(): void
+    {
+        $this->assertEquals(1, 1);
+    }
+}
